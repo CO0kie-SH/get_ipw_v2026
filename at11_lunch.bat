6caa83b7655e8b8834ea6175813e0af6a29e65d0
mhw7 <mhwis666@gmail.com>
2026-04-21 11:06:43 +0800

    26.4.21A

---
+++ at11_lunch.bat
@@ -0,0 +1,12 @@
+@chcp 65001>nul
+@title IP 查看
+@cd /d %~dp0
+@set PYTHONIOENCODING=utf-8
+
+@set path=D:\0Code2\py312;%path%
+@set path=D:\job\py312\Scripts;D:\job\py312;%path%
+
+set fei_title="点饭啦！"
+:: set "only_work=Workday"
+
+python main.py --user2 %*
